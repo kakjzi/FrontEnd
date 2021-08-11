b78bd9ff57d4ab955ca50ed0ea12cb831a8f326c
ShinJiwoo <anmern3@naver.com>
2021-08-11 10:49:28 +0900

    event

---
+++ JS/02.event.js
@@ -0,0 +1,27 @@
+const title = document.querySelector(".heheheheh");
+
+function handleTitleClick(){
+    title.style.color="blue";
+}
+
+
+console.dir(title)
+
+/* 이벤트 등록
+    function handleTitleClick(){
+        title.style.color="blue";
+    }
+
+    1. title.onclick = handleTitleClick
+    2. title.addEventListener("click",handleTitleClick);
+    3. title.addEventListener("click",(event) =>{
+        console.log("Clciked");
+    });
+    4. title.onclick = (event) =>{
+        console.log("Clciked");
+    });
+*/
+console.dir(document.body)
+
+
+
