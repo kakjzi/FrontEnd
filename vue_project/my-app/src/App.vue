aacbfa686767081ea4d937d3327d0a3135e7275b
kakjzi <anmern3@naver.com>
2021-09-20 22:18:21 +0900

    v-model , watch, transition, spread op, lifeCycle.

--- vue_project/my-app/src/App.vue
+++ vue_project/my-app/src/App.vue
@@ -2,12 +2,25 @@
 <template>
 
 <!-- modal -->
-<Modal @close="isShowModal = false"  :isShowModal="isShowModal" :numItem="numItem" :원룸= "원룸" :신고수="신고수" />
-<Discount/>
+<!-- <div class="start" :class="{ end : isShowModal }"> -->
+<transition name="fade">
+  <Modal @close="isShowModal = false" 
+  :isShowModal="isShowModal" 
+  :numItem="numItem" 
+  :원룸= "원룸" 
+  :신고수="신고수" />
+</transition>
+
 
 <div class="menu">
   <a v-for="item in menuItem" :key="item">{{item}}</a>
 </div>
+<Discount v-if="isShowDicount == true" :amount = "amount"/>
+
+<button @click="priceDownSort">가격순정렬(내림차순)</button>
+<button @click="priceUpSort">가격순정렬(오름차순)</button>
+<button @click="productSort">제품정렬</button>
+<button @click="sortBack">되돌리기</button>
 
 <Card @openModal="isShowModal = true; numItem=$event;" @report="신고수[i]++;" v-for="(item,i) in 원룸" :key="i" :isShowModal="isShowModal" :원룸= "원룸[i]" :신고수="신고수[i]"/>
 </template>
@@ -18,19 +31,58 @@ import discount from './components/Discount.vue';
 import modal from './components/Modal.vue';
 import card from './components/Card.vue';
 
+
+
 export default {
   name: 'App',
   data(){
     return {
       numItem : 0 , 
+
+      //배열복사 = slice() , map , [...] (spread operater)
+      원룸오리지널 : [...data],
       원룸 : data,
+      isShowDicount : true,
       isShowModal : false,
       신고수 : [0, 0, 0, 0, 0, 0],
       menuItem : ['Home', 'Shop', 'About'],
+      amount : 30,
     }
   },
-  methods: {
-
+  methods : {
+    //sort -> 원본 변형
+    // map, filter -> 원본 보존 
+    priceDownSort(){
+      this.원룸.sort(function(a,b){
+        return a.price - b.price
+      });
+    },
+    priceUpSort(){
+      this.원룸.sort(function(a,b){
+        return b.price - a.price
+      });
+    },
+    productSort(){
+      this.원룸.sort(function(a,b){
+        return a.title.localeCompare(b.title)
+      });
+    },
+    sortBack(){
+      this.원룸 = [...this.원룸오리지널];
+    }
+  },
+  mounted() {
+    // setTimeout(()=>{
+    //     this.isShowDicount = false
+    // },2000);
+  
+    const timer = setInterval(()=>{
+      if (this.amount == 0){
+        clearInterval(timer);
+      } else{
+        this.amount --;
+      }
+    }, 1000);
   },
   components: {
     Discount : discount,
@@ -42,7 +94,7 @@ export default {
 }
 </script>
 
-<style>
+<style> 
 #app{
   text-align: center;
 }
@@ -90,6 +142,18 @@ div{
     margin: 10px;
     border-radius: 5px;
 }
+
+/* 퇴장 애니메이션 : ??-leave-from     acitve    to ~~ */
+.fade-enter-from{
+  transform: translateY(-1000px);
+}
+.fade-enter-active{
+  transition : all 1s;
+}
+.fade-enter-to{
+  transform: translateY(0px);
+  }
+
 </style>
 
 
--- vue_project/my-app/src/components/Modal.vue
+++ vue_project/my-app/src/components/Modal.vue
@@ -3,8 +3,15 @@
     <div class="white-bg">
         <img :src ="원룸[numItem].image" class = "modal-img">
         <h4>이름 : {{원룸[numItem].title}}</h4>
-        <p>가격 : {{원룸[numItem].price}} 원</p>
         <p>상세설명 : {{원룸[numItem].content}}</p>
+        <!-- <input @input="month = $event.target.value"> -->
+        <input v-model.number="month">
+        <!-- <input type="range" v-model="month" min="1" max="12"> -->
+        <!-- <select v-model="month">
+            <option>213</option>
+        </select> -->
+
+        <p>{{month}} 개월 선택함 : 가격 : {{원룸[numItem].price * month}} 원</p>
         <p>허위매물 신고수 : {{신고수[numItem]}} </p>
         <button @click="$emit('close')">닫기</button>
     </div>
@@ -15,6 +22,28 @@
 <script>
 export default {
     name: 'Modal',
+    data(){
+        return{
+            month : 1,
+        }
+    },
+    // watch : {
+    //     //(a1) a1 변경후 data 
+    //     // (a1 , a2) a1 변경후 : a2 변경전 data
+    //     month(a){
+    //         const checkNum  = /^[0-9]/;
+    //         if (!checkNum.test(a) && isNaN(a) == true){
+    //             alert('정수만 입력해라');
+    //             this.month = 1;
+    //         } 
+    //     }
+    // },
+    beforeUpdate() {
+        if (this.month == 2){
+            alert('2개월 넘 적음');
+            this.month = 3;
+        }
+    },
     props :{
         신고수 : Array,
         원룸 : Object,
